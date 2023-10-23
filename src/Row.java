e4a5e6898883340d00a61215f0916aa59b3947d4
GarrettThing <garrett.clark0103@gmail.com>
2023-10-23 13:00:39 -0500

    added all types in name

--- src/Row.java
+++ src/Row.java
@@ -18,12 +18,32 @@ public class Row {
 
     int typeChance = (int)(Math.random() * 100);
 
-    if (typeChance < 59){
+    if (typeChance < 75){
 
         face = (int)(Math.random() * 6 + 1);
         length = (int)(Math.random()* 6 + 1);
 
         type = length + " of a kind";
+
+    }
+    else if (typeChance < 80){
+
+        type = "small straight";
+
+    }
+    else if (typeChance < 85) {
+
+        type = "large straight";
+
+    }
+    else if (typeChance < 90){
+
+        type = "super straight";
+
+    }
+    else{
+
+        type = "full house";
 
     }
 
