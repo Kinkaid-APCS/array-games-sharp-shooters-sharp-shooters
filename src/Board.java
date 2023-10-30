ba6bf03361704fb6665db3212497f1c69cec29b0
johnklevenhagen <johnklevenhagen@icloud.com>
2023-10-30 13:00:08 -0500

    creating board and rows

--- src/Board.java
+++ src/Board.java
@@ -1,28 +1,50 @@
+
 /**
+ *
  * A Board is a collection of 5-6 Goals, each of which represents a row of spaces.
  */
+
+
+
 public class Board //TESTING COMMIT AND PUSH
 {
 
-
+    public Row[] board;
     public Board()
     {
         // ---------------------------------
         //  TODO: set up the Goals
 
+
+
         // ---------------------------------
 
     }
 
-    public String toString()
+    public void printBoard()
     {
-        String result = "";
 
         // ---------------------------------
         // TODO: modify result so that it shows all the goals.
 
+        for (int z = 0; z < 6; z++)
+        {
+            Row row = new Row();
+        }
+
+        System.out.println("current board");
+        System.out.print("Row 1 : ");
+        for (int z = 0; z < 1; z++) // 1 = how filled
+        {
+            System.out.print(" X ");
+        }
+        for (int x = 6-1; x > 1; x--) // 1 = how filled
+        {
+            System.out.print(" _ ");
+        }
+        System.out.print("                       Reward" + );
         // ---------------------------------
-        return result;
+
 
     }
 
